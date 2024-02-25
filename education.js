db486ee7f73979223025ad7882a4d54254753339
Ruslan <monnaliza139@gmail.com>
2024-02-25 23:13:21 +0200

    update

--- education.js
+++ education.js
@@ -139,4 +139,32 @@
 //     imagesListItem.insertAdjacentHTML('beforeend', murkup)
 // }
 
- renderImages();
+ //renderImages();
+
+// const list = document.querySelector('#categories')
+// const item = list.querySelectorAll('li.item')
+// console.log(`Number of categories : ${item.length}`);
+
+// item.forEach(category => {
+//     const h2 = category.querySelector('h2');
+//     const el = category.querySelectorAll('li')
+
+//     console.log(`Categories: ${h2.textContent}`);
+//     console.log(`Elements: ${el.length}`);
+
+// })
+
+
+// const textInput = document.querySelector('#name-input');
+// const userName = document.querySelector('#name-output');
+
+// textInput.addEventListener("input", handleInput);
+// function handleInput(event) {
+//     const result = event.currentTarget.value;
+//     if (result.trim() === "") {
+//         userName.textContent = "Anonymus";
+//     }
+//     else {
+//         userName.textContent = result;
+//     }
+// }
