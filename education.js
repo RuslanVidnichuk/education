5ec090f845a266484784845d5ceced3a24d8665c
Ruslan <monnaliza139@gmail.com>
2024-02-28 22:55:16 +0200

    update_5

--- education.js
+++ education.js
@@ -168,3 +168,10 @@
 //         userName.textContent = result;
 //     }
 // }
+// const instance = basicLightbox.create(`
+// 	<h1>Not closable</h1>
+// 	<p>It's not possible to close this lightbox with a click.</p>
+// `, {
+// 	closable: false
+// })
+// instance.show()
